1d03dff34a5059fb2897bda54c36f651f204fdad
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-23 16:20:54 +0300

    add product service

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -13,18 +13,23 @@ class Api {
           'there is a problem with status code ${response.statusCode}');
     }
   }
-}
 
-Future<dynamic> post(
-    {required String url,
-    @required dynamic body,
-    @required String? token}) async {
-  Map<String, String> headers = {};
-  if (token != null) {
-    headers.addAll({'Authorization': 'Bearer $token'});
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception('there is a problem with status code ${response.statusCode} with body ${jsonDecode(response.body)}' );
+    }
   }
-  http.Response response =
-      await http.post(Uri.parse(url), body: body, headers: headers);
-  Map<String, dynamic> data = jsonDecode(response.body);
-  return data;
 }
