8267acf96cb39327f9e96cd88257af3569ddfcee
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-23 15:57:36 +0300

    add post method

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -7,9 +7,24 @@ class Api {
     http.Response response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
-      return jsonDecode(response.body) ;
-    }else{
-       throw Exception('there is a problem with status code ${response.statusCode}');
+      return jsonDecode(response.body);
+    } else {
+      throw Exception(
+          'there is a problem with status code ${response.statusCode}');
     }
   }
+}
+
+Future<dynamic> post(
+    {required String url,
+    @required dynamic body,
+    @required String? token}) async {
+  Map<String, String> headers = {};
+  if (token != null) {
+    headers.addAll({'Authorization': 'Bearer $token'});
+  }
+  http.Response response =
+      await http.post(Uri.parse(url), body: body, headers: headers);
+  Map<String, dynamic> data = jsonDecode(response.body);
+  return data;
 }
