10f7bf1fc07cde4935af7007914a69b24d34485d
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-23 17:25:43 +0300

    create card

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,15 +10,60 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-         IconButton(onPressed: (){}, icon:  Icon(
-            FontAwesomeIcons.cartPlus,
-            color: Colors.black,
-          ))
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.cartPlus,
+                color: Colors.black,
+              ))
         ],
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
         title: Text('New Trend'),
+      ),
+      body: Container(
+        height: 130,
+        width: 180,
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              blurRadius: 40,
+              color: Colors.grey.withOpacity(.2),
+              spreadRadius: 0,
+              offset: Offset(10, 10)),
+        ]),
+        child: Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'HandBag Lv',
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      r'$225',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
